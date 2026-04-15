b574f6f85faed749c70d0dec1386b8be29055043
Netoperer <netoperer@users.noreply.github.com>
2026-04-15 21:03:28 +0200

    feat: make it render

---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+// import "./app.css";
+//
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter } from "react-router";
+// import App from "./app";
+//
+// const root = document.getElementById("root")!;
+//
+// ReactDOM.createRoot(root).render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>,
+// );
+// i
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { reactRouter } from "@react-router/dev/vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [tailwindcss(), react()],
   resolve: {
     tsconfigPaths: true,
   },
