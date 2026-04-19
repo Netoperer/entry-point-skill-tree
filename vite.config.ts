164b678b48ca6bde265982e608d6a3e20999639e
Netoperer <netoperer@users.noreply.github.com>
2026-04-19 23:14:54 +0200

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   resolve: {
     tsconfigPaths: true,
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
