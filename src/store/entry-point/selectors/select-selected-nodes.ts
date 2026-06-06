9ebd2c4a732c1934254b95f87585ac2cd1792033
Netoperer <netoperer@users.noreply.github.com>
2026-06-06 14:17:29 +0200

    refactor: split selectors into separate files

---
+++ src/store/entry-point/selectors/select-selected-nodes.ts
@@ -0,0 +1,47 @@
+import { createSelector } from "reselect";
+import type { StoreState } from "..";
+import { EntryPointGraph } from "@/core/entry-point/graph";
+import { getUnlockableNodes } from "@/core/entry-point/can-unlock-node";
+import { getNodesToRemove } from "@/core/entry-point/get-nodes-to-remove";
+
+const getHoveredNode = (state: StoreState) => state.hoveredNode;
+const getUnlockedNodes = (state: StoreState) => state.unlockedNodes;
+const getStarterClass = (state: StoreState) => state.starterClass;
+const getPerkLimit = (state: StoreState) => state.perkLimit;
+
+export const selectSelectedNodes = createSelector(
+  [getHoveredNode, getUnlockedNodes, getStarterClass, getPerkLimit],
+  (hoveredNode, unlockedNodes, starterClass, perkLimit) => {
+    if (!hoveredNode || hoveredNode === starterClass) {
+      return new Set<string>();
+    }
+
+    const isLocked = !unlockedNodes.has(hoveredNode);
+
+    if (isLocked) {
+      const path = EntryPointGraph.pathToClosestUnlocked(
+        hoveredNode,
+        unlockedNodes,
+        perkLimit,
+      );
+      if (!path) {
+        return new Set<string>();
+      }
+
+      const unlockableNodes = getUnlockableNodes(
+        unlockedNodes,
+        perkLimit,
+        path,
+      );
+
+      return new Set(unlockableNodes);
+    } else {
+      const nodesToLock = getNodesToRemove(
+        hoveredNode,
+        unlockedNodes,
+        starterClass,
+      );
+      return nodesToLock;
+    }
+  },
+);
